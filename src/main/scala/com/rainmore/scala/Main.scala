3ef0a76eea4ab71540954d1d5feacb48546587d1
Felix Jie Rong <felixr@worldmanager.com>
2013-10-28 15:18:37 +1100

    run db

--- src/main/scala/com/rainmore/scala/Main.scala
+++ src/main/scala/com/rainmore/scala/Main.scala
@@ -10,30 +10,27 @@ object Main extends App {
   val injector: Injector = Guice.createInjector(new PersistenceModule, new JpaPersistModule("JpaBasicsTutorial"))
   val app: ApplicationInitializer = injector.getInstance(classOf[ApplicationInitializer])
 
-  val emf: EntityManagerFactory = Persistence.createEntityManagerFactory("JpaBasicsTutorial")
-  val em: EntityManager = emf.createEntityManager()
-  val service: PersonService = new PersonService(em)
+  val example = injector.getInstance(classOf[Example]).run()
 
+  // this is to test @Provides will create a new instance
   val h1: HelloWorld = injector.getInstance(classOf[HelloWorld])
   val h2: HelloWorld = injector.getInstance(classOf[HelloWorld])
   System.out.println(h1.toString)
-
-
   System.out.println(h2.toString)
+
+  // this is to test @Singleton return the same instance
   val hs1: HelloWorldSingleton = injector.getInstance(classOf[HelloWorldSingleton])
   val hs2: HelloWorldSingleton = injector.getInstance(classOf[HelloWorldSingleton])
   System.out.println(hs1.toString)
-
-
   System.out.println(hs2.toString)
+
+  // return same instance of PersonService
   val p1: PersonService = injector.getInstance(classOf[PersonService])
   val p2: PersonService = injector.getInstance(classOf[PersonService])
   System.out.println(p1.toString)
-
-
   System.out.println(p2.toString)
 
-  emf.close()
+  app.stop
 
   def prPerson(person: Person): Unit = {
     println(person.toString)
